72f378dc09397165ce87341d3c112475e3d47848
Sunconure11 <eyespot7b@aol.com>
2017-02-25 17:07:33 -0500

    apparently im awful enough that i can't code a damn block...
    oh well, ill work on honing it later, sleep is fucked

---
+++ src/main/java/wiccanArts/block/BlockMoldaviteOre.java
@@ -0,0 +1,11 @@
+package wiccanArts.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+/**
+ * Created by Joseph on 2/25/2017.
+ */
+public class BlockMoldaviteOre {
+}
