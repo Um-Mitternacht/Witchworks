19f0b88edf496912cce3568598545e16e08650df
Sunconure11 <eyespot7b@aol.com>
2017-02-27 21:13:42 -0500

    last ride of the day
    We live in every moment but this one
    Why don't we recognise the faces loving us so
    
    What's God if not the spark that started life
    Smile of a stranger
    Sweet music, starry skies
    Wonder, mystery, wherever my road goes
    Early wake-ups in a moving home
    scent of fresh-mown grass in the morning sun
    Open theme park gates waiting for
    
    Riding the day, every day into sunset
    Finding the way back home
    
    Once upon a night we'll wake to the carnival of life
    the beauty of this ride ahead such an incredible high
    It's hard to light a candle, easy to curse the dark instead
    This moment the dawn of humanity
    The last ride of the day
    
    Wake up, dead boy
    Enter adventureland
    Tricksters, magicians will show you all that's real
    Careless jugglers, snake charmers by your trail
    Magic of a moment
    Abracadabra

--- src/main/java/com/wiccanarts/api/enums/WAOres.java
+++ src/main/java/com/wiccanarts/api/enums/WAOres.java
@@ -6,14 +6,14 @@ import net.minecraft.util.IStringSerializable;
  * Created by Joseph on 2/27/2017.
  */
 public enum WAOres implements IStringSerializable {
-    AMETHYST, NUUMMITE, MOLDAVITE, SERPENTINE, ALEXANDRITE, OVERQUARTZ, GARNET, JASPER, MALACHITE, TOURMALINE, TIGERSEYE, PETOSKEYSTONE, BLOODSTONE, SALT, SILVER, CHIMAERICMETAL;
+	AMETHYST, NUUMMITE, MOLDAVITE, SERPENTINE, ALEXANDRITE, OVERQUARTZ, GARNET, JASPER, MALACHITE, TOURMALINE, TIGERSEYE, PETOSKEYSTONE, BLOODSTONE, SALT, SILVER, CHIMAERICMETAL;
 
-    public String getName() {
-        return this.name().toLowerCase();
-    }
+	public String getName() {
+		return this.name().toLowerCase();
+	}
 
-    @Override
-    public String toString() {
-        return this.getName();
-    }
+	@Override
+	public String toString() {
+		return this.getName();
+	}
 }
