feb8ad3aa8508b4d06867e41e715e5523d2deff7
Sunconure11 <eyespot7b@aol.com>
2017-02-27 19:49:03 -0500

    set this up as a dummy for now

--- src/main/java/com/wiccanarts/common/block/BlockOre.java
+++ src/main/java/com/wiccanarts/common/block/BlockOre.java
@@ -1,10 +1,17 @@
 package com.wiccanarts.common.block;
 
 import com.wiccanarts.common.lib.LibBlockName;
+import com.wiccanarts.api.enums.WAOres;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
 
 /**
  * Created by Joseph on 2/27/2017.
  */
-public class BlockOre {
+public class BlockOre extends Block {
 
+    public BlockOre(Material blockMaterialIn, MapColor blockMapColorIn) {
+        super(blockMaterialIn, blockMapColorIn);
+    }
 }
