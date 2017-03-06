cccbb1ef341043c6f38982cc1b3a4cb5c2f85f6e
Sunconure11 <eyespot7b@aol.com>
2017-03-06 17:07:46 -0500

    reformatting

--- src/main/java/com/wiccanarts/common/block/tools/BlockKettle.java
+++ src/main/java/com/wiccanarts/common/block/tools/BlockKettle.java
@@ -4,7 +4,6 @@ import com.wiccanarts.api.item.IModelRegister;
 import com.wiccanarts.client.handler.ModelHandler;
 import com.wiccanarts.common.block.BlockMod;
 import com.wiccanarts.common.lib.LibBlockName;
-import javafx.geometry.BoundingBox;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
@@ -17,10 +16,10 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 /**
  * Created by Joseph on 3/4/2017.
  */
-public class BlockKettle extends BlockMod implements IModelRegister{
+public class BlockKettle extends BlockMod implements IModelRegister {
 	//public static final int FIRE_TICKS = 20 * 5;//for the "5 seconds above fire in order to heat up" thing. can be increased.
 
-	private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(1*0.0625, 0, 1*0.0625, 15*0.0625, 11*0.0625, 15*0.0625);
+	private static final AxisAlignedBB BOUNDING_BOX = new AxisAlignedBB(1 * 0.0625, 0, 1 * 0.0625, 15 * 0.0625, 11 * 0.0625, 15 * 0.0625);
 
 	public BlockKettle() {
 		super(LibBlockName.KETTLE, Material.IRON);
