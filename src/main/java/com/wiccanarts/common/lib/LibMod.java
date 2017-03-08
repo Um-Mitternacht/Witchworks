1b5e2c83565b208449fb2372b4e23a9027627339
Sunconure11 <eyespot7b@aol.com>
2017-03-07 20:46:12 -0500

    whoops

--- src/main/java/com/wiccanarts/common/lib/LibMod.java
+++ src/main/java/com/wiccanarts/common/lib/LibMod.java
@@ -17,7 +17,7 @@ public final class LibMod {
 	public static final String MOD_VER = "@VERSION@";
 
 	//Dependency
-	public static final String DEPENDENCIES = "required-after:Forge@[12.18.3.2234,];required-after:JEI@[3.14.7.415,];required-after:HWYLA@[1.8.10-B24_1.10.2,];required-after:llibrary@[1.7.4,];required-after:Baubles@[1.3.9,]";
+	public static final String DEPENDENCIES = "required-after:Forge@[12.18.3.2234,];required-after:JEI@[3.14.7.415,];required-after:WAILA@[1.8.10-B24_1.10.2,];required-after:llibrary@[1.7.4,];required-after:Baubles@[1.3.9,]";
 
 	//Client proxy location
 	public static final String PROXY_CLIENT = "com.wiccanarts.client.core.ClientProxy";
