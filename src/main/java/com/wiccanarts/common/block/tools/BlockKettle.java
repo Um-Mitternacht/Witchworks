c7e6129b31649537724bd5cd46680ba9f9c87429
Sunconure11 <eyespot7b@aol.com>
2017-03-28 12:57:42 -0400

    see if it rotates now

--- src/main/java/com/wiccanarts/common/block/tools/BlockKettle.java
+++ src/main/java/com/wiccanarts/common/block/tools/BlockKettle.java
@@ -99,8 +99,8 @@ public class BlockKettle extends BlockMod implements IModelRegister, ITileEntity
 
 	@SuppressWarnings("deprecation")
 	@Override
-	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-		IBlockState iblockstate = super.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
+	public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+		IBlockState iblockstate = super.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
 		iblockstate = iblockstate.withProperty(FACING, placer.getHorizontalFacing());
 		return facing != EnumFacing.DOWN && (facing == EnumFacing.UP || hitY <= 0.5F) ?
 				iblockstate.withProperty(HALF, BlockStairs.EnumHalf.BOTTOM) :
