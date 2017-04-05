e0d637fbfd67400fc28d0a29e8e036297a8d47a1
Sunconure11 <eyespot7b@aol.com>
2017-04-05 04:40:42 -0400

    what even happened here

--- src/main/java/com/wiccanarts/common/core/CommonRegistration.java
+++ src/main/java/com/wiccanarts/common/core/CommonRegistration.java
@@ -59,7 +59,7 @@ public class CommonRegistration {
 		registerCrop(MANDRAKE_ROOT, ModBlocks.CROP_MANDRAKE_ROOT, new ItemMandrakeRoot(), LibItemName.SEED_MANDRAKE_ROOT);
 		registerCrop(LILAC, ModBlocks.CROP_LILAC, new ItemLilac(), LibItemName.SEED_LILAC);
 		registerCrop(LAVENDER, ModBlocks.CROP_LAVENDER, new ItemLavender(), LibItemName.SEED_LAVENDER);
-		registerCrop(THISTLE, ModBlocks.CROP_THISTLE, new ItemThirstle(), LibItemName.SEED_THISTLE);
+		registerCrop(THISTLE, ModBlocks.CROP_THISTLE, new ItemThistle(), LibItemName.SEED_THISTLE);
 
 		CropRegistry.getFoods().forEach((crop, item) -> event.getRegistry().register(item));
 		CropRegistry.getSeeds().forEach((crop, item) -> event.getRegistry().register(item));
