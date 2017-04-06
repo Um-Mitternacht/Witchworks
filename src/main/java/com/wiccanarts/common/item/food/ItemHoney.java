bc1e4292ccf68249629fbe229d195015cff1243f
Sunconure11 <eyespot7b@aol.com>
2017-04-06 09:05:27 -0400

    honey tweaks
    planning on glass jars for container item later

--- src/main/java/com/wiccanarts/common/item/food/ItemHoney.java
+++ src/main/java/com/wiccanarts/common/item/food/ItemHoney.java
@@ -2,8 +2,10 @@ package com.wiccanarts.common.item.food;
 
 import com.wiccanarts.api.item.IModelRegister;
 import com.wiccanarts.client.handler.ModelHandler;
+import com.wiccanarts.common.item.ModItems;
 import com.wiccanarts.common.lib.LibItemName;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
@@ -22,11 +24,10 @@ public class ItemHoney extends ItemModFood implements IModelRegister {
 
 	@Override
 	protected void onFoodEaten (ItemStack stack, World worldIn, EntityPlayer player) {
-		player.addPotionEffect (new PotionEffect (MobEffects.HASTE, 100, 0));
-		player.addPotionEffect (new PotionEffect (MobEffects.LUCK, 100, 0));
+		player.addPotionEffect (new PotionEffect (MobEffects.HASTE, 450, 0));
+		player.addPotionEffect (new PotionEffect (MobEffects.LUCK, 450, 0));
 	}
 
-	//FIXME: Help me fix the textures
 	@Override
 	public void registerModels () {
 		ModelHandler.registerItem (this);
