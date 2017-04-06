1f66d00300c9085662a3f3f8dccd26c32b95ed9b
Sunconure11 <eyespot7b@aol.com>
2017-04-06 03:37:27 -0400

    fixme

--- src/main/java/com/wiccanarts/common/item/food/ItemHoney.java
+++ src/main/java/com/wiccanarts/common/item/food/ItemHoney.java
@@ -1,5 +1,6 @@
 package com.wiccanarts.common.item.food;
 
+import com.wiccanarts.api.item.IModelRegister;
 import com.wiccanarts.common.lib.LibItemName;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
@@ -7,22 +8,26 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
-import static com.wiccanarts.common.lib.LibItemName.HONEY;
-
 /**
  * This class was created by Joseph on 3/4/2017.
  * It's distributed as part of Wiccan Arts under
  * the MIT license.
  */
-public class ItemHoney extends ItemModFood {
+public class ItemHoney extends ItemModFood implements IModelRegister {
 
 	public ItemHoney () {
-		super (HONEY, 2, 4F, false);
+		super (LibItemName.HONEY, 2, 4F, false);
 	}
 
 	@Override
 	protected void onFoodEaten (ItemStack stack, World worldIn, EntityPlayer player) {
-		player.addPotionEffect (new PotionEffect (MobEffects.HASTE, 50, 0));
-		player.addPotionEffect (new PotionEffect (MobEffects.LUCK, 50, 0));
+		player.addPotionEffect (new PotionEffect (MobEffects.HASTE, 100, 0));
+		player.addPotionEffect (new PotionEffect (MobEffects.LUCK, 100, 0));
+	}
+
+	//FIXME: Help me fix the textures
+	@Override
+	public void registerModels () {
+
 	}
 }
