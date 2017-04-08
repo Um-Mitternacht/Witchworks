21eb426bb9452cbd41a718903376363fbf258386
Sunconure11 <eyespot7b@aol.com>
2017-04-08 12:52:27 -0400

    toying around with the json a bit

--- src/main/java/com/wiccanarts/common/block/natural/BlockBeeHive.java
+++ src/main/java/com/wiccanarts/common/block/natural/BlockBeeHive.java
@@ -8,7 +8,6 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
