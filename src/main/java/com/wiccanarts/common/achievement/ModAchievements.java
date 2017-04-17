2a90ad0000bfc751f2203ae8b811c3bfbf010f23
Sunconure11 <eyespot7b@aol.com>
2017-04-17 17:49:10 -0400

    now i need to figure out how to apply this to the item

--- src/main/java/com/wiccanarts/common/achievement/ModAchievements.java
+++ src/main/java/com/wiccanarts/common/achievement/ModAchievements.java
@@ -1,6 +1,7 @@
 package com.wiccanarts.common.achievement;
 
 import com.wiccanarts.common.block.ModBlocks;
+import com.wiccanarts.common.item.ModItems;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.Achievement;
@@ -9,9 +10,14 @@ import net.minecraftforge.common.MinecraftForge;
 import com.wiccanarts.common.lib.LibAchievementName;
 
 /**
- * Created by Joseph on 4/17/2017.
+ * Created by Joseph on 4/17/2017. Original code by Vazkii from Botania.
  */
 public final class ModAchievements {
 
 	public static Achievement malachiteCraft;
+
+	public static void init() {
+
+		malachiteCraft = new AchievementMod(LibAchievementName.MALACHITE_CRAFT, 0, 11, ModItems.MALACHITE, malachiteCraft);
+	}
 }
