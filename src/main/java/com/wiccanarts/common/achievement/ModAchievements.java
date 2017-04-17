511b703306940174f4e7a983440d96f1e91cae99
Sunconure11 <eyespot7b@aol.com>
2017-04-17 16:42:05 -0400

    starting work on achievements... slowly
    sleep is a bit damaged still, will work on this over the course of the
    next few days

--- src/main/java/com/wiccanarts/common/achievement/ModAchievements.java
+++ src/main/java/com/wiccanarts/common/achievement/ModAchievements.java
@@ -1,7 +1,17 @@
 package com.wiccanarts.common.achievement;
 
+import com.wiccanarts.common.block.ModBlocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Achievement;
+import net.minecraftforge.common.AchievementPage;
+import net.minecraftforge.common.MinecraftForge;
+import com.wiccanarts.common.lib.LibAchievementName;
+
 /**
  * Created by Joseph on 4/17/2017.
  */
 public final class ModAchievements {
+
+	public static Achievement malachiteCraft;
 }
---
+++ src/main/java/com/wiccanarts/common/lib/LibAchievementName.java
@@ -0,0 +1,9 @@
+package com.wiccanarts.common.lib;
+
+/**
+ * Created by Joseph on 4/17/2017.
+ */
+public final class LibAchievementName {
+
+	public static final String MALACHITE_CRAFT = "malachiteCraft";
+}
