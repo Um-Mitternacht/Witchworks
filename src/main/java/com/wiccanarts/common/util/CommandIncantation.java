3da0263496a7c013b0de2424b1023e36b24b9a9f
Sunconure11 <eyespot7b@aol.com>
2017-04-19 17:00:56 -0400

    reformatting

--- src/main/java/com/wiccanarts/common/util/CommandIncantation.java
+++ src/main/java/com/wiccanarts/common/util/CommandIncantation.java
@@ -48,8 +48,7 @@ public class CommandIncantation implements ICommand {
 		EntityPlayer caster;
 		caster = (EntityPlayer) sender.getCommandSenderEntity();
 		if (caster == null) return;
-		if (args[0].equalsIgnoreCase("BURN!"))
-		{
+		if (args[0].equalsIgnoreCase("BURN!")) {
 			RayTraceResult rez = caster.rayTrace(3, 1.0F);
 			EnumFacing side = rez.sideHit;
 			BlockPos block = rez.getBlockPos();
@@ -57,7 +56,7 @@ public class CommandIncantation implements ICommand {
 				caster.getEntityWorld().playSound(null, block, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.BLOCKS, 1.0F, 2F);
 				caster.getEntityWorld().setBlockState(block.offset(side), Blocks.FIRE.getDefaultState());
 			}
-		} else if (args[0].equalsIgnoreCase("HEAL!"));
+		} else if (args[0].equalsIgnoreCase("HEAL!")) ;
 		{
 			caster = (EntityPlayer) sender.getCommandSenderEntity();
 			caster.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 500, 0));
