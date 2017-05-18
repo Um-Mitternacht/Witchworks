33cca4ec0764a16ba6c7174080aff0260c8c6d2e
Sunconure11 <eyespot7b@aol.com>
2017-05-17 22:24:39 -0400

    very minor achievement tweaks

--- src/main/java/com/witchworks/common/lib/LibAchievementName.java
+++ src/main/java/com/witchworks/common/lib/LibAchievementName.java
@@ -7,5 +7,9 @@ package com.witchworks.common.lib;
  */
 public final class LibAchievementName {
 
+	//Easter Eggs
 	public static final String MALACHITE_CRAFT = "craftMalachite";
+
+	//Mainline Achievements (to be implemented)
+
 }
