fb7f11f35a53f6a17a5b67d781cf8941be643886
Sunconure11 <eyespot7b@aol.com>
2017-05-19 10:26:01 -0400

    i appear to have fried my memory from not sleeping enough this night

--- src/main/java/com/witchworks/common/item/magic/tools/ItemTaglock.java
+++ src/main/java/com/witchworks/common/item/magic/tools/ItemTaglock.java
@@ -1,24 +1,32 @@
 package com.witchworks.common.item.magic.tools;
 
+import com.witchworks.api.item.NBTHelper;
 import com.witchworks.common.item.ItemMod;
 import com.witchworks.common.lib.LibItemName;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.TextFormatting;
 
-import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Joseph on 5/15/2017.
  */
 public class ItemTaglock extends ItemMod {
 
-		public ItemTaglock() {
-			super(LibItemName.TAGLOCK);
-		}
+	public ItemTaglock() {
+		super(LibItemName.TAGLOCK);
+	}
+
+	public void getEntityTag(ItemStack stack, UUID uuid) {
+
+		NBTHelper.setUniqueID(stack, "name", uuid);
+	}
+
 
 	public void addInformation() {
+
+	}
+
+	public void setTaggedEntity() {
 
 	}
 }
