c5c5a6801c98d06a2e5d2d1917aa6883c1258dc4
Sunconure11 <eyespot7b@aol.com>
2017-05-19 10:45:19 -0400

    rework a bit after the sandman stops trying to hurt me

--- src/main/java/com/witchworks/common/item/magic/tools/ItemTaglock.java
+++ src/main/java/com/witchworks/common/item/magic/tools/ItemTaglock.java
@@ -3,9 +3,16 @@ package com.witchworks.common.item.magic.tools;
 import com.witchworks.api.item.NBTHelper;
 import com.witchworks.common.item.ItemMod;
 import com.witchworks.common.lib.LibItemName;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 
-import java.util.UUID;
+import static net.minecraft.util.math.RayTraceResult.Type.ENTITY;
 
 /**
  * Created by Joseph on 5/15/2017.
@@ -16,17 +23,14 @@ public class ItemTaglock extends ItemMod {
 		super(LibItemName.TAGLOCK);
 	}
 
-	public void getEntityTag(ItemStack stack, UUID uuid) {
-
-		NBTHelper.setUniqueID(stack, "name", uuid);
-	}
-
-
-	public void addInformation() {
-
-	}
-
-	public void setTaggedEntity() {
-
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStack, World world, EntityPlayer entityPlayer, EnumHand hand) {
+		RayTraceResult result = ProjectileHelper.forwardsRaycast(entityPlayer, true, true, entityPlayer);
+		ActionResult<ItemStack> actionResult = ActionResult.newResult(EnumActionResult.PASS, itemStack);
+		if (result.typeOfHit == ENTITY) {
+			NBTHelper.setUniqueID(itemStack, "name", result.entityHit.getUniqueID());
+			actionResult = ActionResult.newResult(EnumActionResult.SUCCESS, itemStack);
+		}
+		return actionResult;
 	}
 }
