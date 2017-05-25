4047ad1e3c270df42affbf296805d449f27f3bb4
Sunconure11 <eyespot7b@aol.com>
2017-05-25 06:31:54 -0400

    made salt work once more

--- src/main/java/com/witchworks/common/item/block/ItemSalt.java
+++ src/main/java/com/witchworks/common/item/block/ItemSalt.java
@@ -4,7 +4,9 @@ import com.witchworks.common.block.ModBlocks;
 import com.witchworks.common.item.ItemMod;
 import com.witchworks.common.lib.LibItemName;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
@@ -23,16 +25,21 @@ public class ItemSalt extends ItemMod {
 		super(LibItemName.SALT);
 	}
 
-	public EnumActionResult onItemUse(Block block, ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-		final boolean flag = worldIn.getBlockState(pos).getBlock().isReplaceable(worldIn, pos);
-		final BlockPos blockpos = flag ? pos : pos.offset(facing);
+	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		{
+			boolean flag = worldIn.getBlockState(pos).getBlock().isReplaceable(worldIn, pos);
+			BlockPos blockpos = flag ? pos : pos.offset(facing);
+			ItemStack itemstack = player.getHeldItem(hand);
 
-		if (playerIn.canPlayerEdit(blockpos, facing, stack) && block.canPlaceBlockAt(worldIn, pos) && ModBlocks.SALT_BARRIER.canPlaceBlockAt(worldIn, blockpos)) {
-			stack.getItem();
-			worldIn.setBlockState(blockpos, ModBlocks.SALT_BARRIER.getDefaultState());
-			return EnumActionResult.SUCCESS;
-		} else {
-			return EnumActionResult.FAIL;
+			if (player.canPlayerEdit(blockpos, facing, itemstack) && worldIn.mayPlace(worldIn.getBlockState(blockpos).getBlock(), blockpos, false, facing, (Entity) null) && ModBlocks.SALT_BARRIER.canPlaceBlockAt(worldIn, blockpos)) {
+				itemstack.shrink(1);
+				worldIn.setBlockState(blockpos, ModBlocks.SALT_BARRIER.getDefaultState());
+				return EnumActionResult.SUCCESS;
+			}
+			else
+			{
+				return EnumActionResult.FAIL;
+			}
 		}
 	}
 }
