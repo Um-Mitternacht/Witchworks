c7bfa90cda273c590f800c2e0b894a04edb2c175
Sunconure11 <eyespot7b@aol.com>
2017-07-15 06:13:41 -0400

    Athame is semi-functional

--- src/main/java/com/witchworks/common/item/tool/ItemAthame.java
+++ src/main/java/com/witchworks/common/item/tool/ItemAthame.java
@@ -3,19 +3,26 @@ package com.witchworks.common.item.tool;
 import com.witchworks.api.helper.IModelRegister;
 import com.witchworks.client.handler.ModelHandler;
 import com.witchworks.common.core.WitchWorksCreativeTabs;
+import com.witchworks.common.core.helper.ItemNBTHelper;
 import com.witchworks.common.item.ModMaterials;
 import com.witchworks.common.lib.LibItemName;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.*;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
 
 /**
  * This class was created by BerciTheBeast on 27.3.2017.
@@ -45,6 +52,29 @@ public class ItemAthame extends ItemSword implements IModelRegister {
 				stack.damageItem(1, attacker);
 			}
 		return true;
+	}
+
+	@SubscribeEvent
+	public void onEntityDrops(LivingDropsEvent event) {
+		if(event.isRecentlyHit() && event.getSource().getTrueSource() != null && event.getSource().getTrueSource() instanceof EntityPlayer) {
+			ItemStack weapon = ((EntityPlayer) event.getSource().getTrueSource()).getHeldItemMainhand();
+			if(!weapon.isEmpty() && weapon.getItem() == this) {
+				Random rand = event.getEntityLiving().world.rand;
+				int looting = EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, weapon);
+
+				if(event.getEntityLiving() instanceof AbstractSkeleton && rand.nextInt(26) <= 3 + looting)
+					addDrop(event, new ItemStack(Items.SKULL, 1, event.getEntityLiving() instanceof EntityWitherSkeleton ? 1 : 0));
+				else if(event.getEntityLiving() instanceof EntityZombie && !(event.getEntityLiving() instanceof EntityPigZombie) && rand.nextInt(26) <= 2 + 2 * looting)
+					addDrop(event, new ItemStack(Items.SKULL, 1, 2));
+				else if(event.getEntityLiving() instanceof EntityCreeper && rand.nextInt(26) <= 2 + 2 * looting)
+					addDrop(event, new ItemStack(Items.SKULL, 1, 4));
+				else if(event.getEntityLiving() instanceof EntityPlayer && rand.nextInt(11) <= 1 + looting) {
+					ItemStack stack = new ItemStack(Items.SKULL, 1, 3);
+					ItemNBTHelper.setString(stack, "SkullOwner", event.getEntityLiving().getName());
+					addDrop(event, stack);
+				}
+			}
+		}
 	}
 
 	private void addDrop(LivingDropsEvent event, ItemStack drop) {
