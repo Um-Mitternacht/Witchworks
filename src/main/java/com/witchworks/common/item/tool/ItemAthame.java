81e21b518c6b3b58cb83161888a4d38cc232331d
Sunconure11 <eyespot7b@aol.com>
2017-07-15 05:53:35 -0400

    Don't code when you need to sleep.

--- src/main/java/com/witchworks/common/item/tool/ItemAthame.java
+++ src/main/java/com/witchworks/common/item/tool/ItemAthame.java
@@ -3,22 +3,37 @@ package com.witchworks.common.item.tool;
 import com.witchworks.api.helper.IModelRegister;
 import com.witchworks.client.handler.ModelHandler;
 import com.witchworks.common.core.WitchWorksCreativeTabs;
+import com.witchworks.common.item.ModItems;
 import com.witchworks.common.item.ModMaterials;
 import com.witchworks.common.lib.LibItemName;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.passive.EntityBat;
+import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Enchantments;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.util.DamageSource;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
 
 /**
  * This class was created by BerciTheBeast on 27.3.2017.
  * It's distributed as part of Witchworks under
  * the MIT license.
  */
+// Uses code from Botania
+
 public class ItemAthame extends ItemSword implements IModelRegister {
 
 	public ItemAthame() {
@@ -26,20 +41,26 @@ public class ItemAthame extends ItemSword implements IModelRegister {
 		setRegistryName(LibItemName.ATHAME);
 		setUnlocalizedName(LibItemName.ATHAME);
 		setCreativeTab(WitchWorksCreativeTabs.ITEMS_CREATIVE_TAB);
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 
-	//Todo: Make athame pull from a set of specialized loot tables, for mob harvests.
 	@Override
 	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
 		if (!target.world.isRemote)
 			if (target instanceof EntityEnderman && attacker instanceof EntityPlayer) {
 				target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), 20);
-				stack.damageItem(100, attacker);
+				stack.damageItem(50, attacker);
 			} else {
 				stack.damageItem(1, attacker);
 			}
 		return true;
+	}
+
+	private void addDrop(LivingDropsEvent event, ItemStack drop) {
+		EntityItem entityitem = new EntityItem(event.getEntityLiving().world, event.getEntityLiving().posX, event.getEntityLiving().posY, event.getEntityLiving().posZ, drop);
+		entityitem.setPickupDelay(10);
+		event.getDrops().add(entityitem);
 	}
 
 	@SideOnly(Side.CLIENT)
