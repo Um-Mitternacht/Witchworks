ab354a0c153714387d5b8d01cde16e433afc9470
Sunconure11 <eyespot7b@aol.com>
2017-08-03 15:17:31 -0400

    Fixed oregen

--- src/main/java/com/witchworks/common/core/gen/WorldGenOre.java
+++ src/main/java/com/witchworks/common/core/gen/WorldGenOre.java
@@ -5,7 +5,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.state.pattern.BlockMatcher;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.common.BiomeDictionary;
