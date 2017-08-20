bbed2777032d43a9a59fe6e7ffa040da048600cb
Sunconure11 <eyespot7b@aol.com>
2017-08-20 16:09:58 -0400

    Return of sapling attempt, heavy WIP
    Also more tweaks of brew of ice world

---
+++ src/main/java/com/witchworks/common/item/block/ItemSapling.java
@@ -0,0 +1,59 @@
+package com.witchworks.common.item.block;
+
+import com.witchworks.api.helper.IModelRegister;
+import com.witchworks.client.handler.ModelHandler;
+import com.witchworks.common.block.natural.BlockGemOre;
+import com.witchworks.common.block.natural.tree.BlockWitchSapling;
+import com.witchworks.common.core.WitchWorksCreativeTabs;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+import javax.annotation.Nonnull;
+
+/**
+ * This class was created by <Arekkuusu> on 27/06/2017.
+ * It's distributed as part of Solar Epiphany under
+ * the MIT license.
+ */
+public class ItemSapling extends ItemBlock implements IModelRegister {
+
+	public ItemSapling(Block block) {
+		super(block);
+		setRegistryName(block.getRegistryName());
+		setHasSubtypes(true);
+		setMaxDamage(0);
+		this.setCreativeTab(WitchWorksCreativeTabs.PLANTS_CREATIVE_TAB);
+	}
+
+	@Override
+	public int getMetadata(int damage) {
+		return damage;
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack stack) {
+		return super.getUnlocalizedName() + "_" + BlockWitchSapling.sapling.values()[stack.getMetadata()].getName();
+	}
+
+	@Override
+	public void getSubItems(@Nonnull CreativeTabs tab, @Nonnull NonNullList<ItemStack> items) {
+		if (this.isInCreativeTab(tab)) {
+			for (int i = 0; i < BlockWitchSapling.sapling.values().length; ++i) {
+				items.add(new ItemStack(this, 1, i));
+			}
+		}
+	}
+
+
+	@Override
+	public void registerModel() {
+		BlockWitchSapling.sapling[] values = BlockWitchSapling.sapling.values();
+		for (int i = 0; i < values.length; i++) {
+			BlockWitchSapling.sapling sapling = values[i];
+			ModelHandler.registerForgeModel(this, i, "gem=" + sapling.getName());
+		}
+	}
+}
