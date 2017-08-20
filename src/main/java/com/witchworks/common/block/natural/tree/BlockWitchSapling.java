e95bc6c8efaaf213a5141c56b1b8b276900bc7fa
Sunconure11 <eyespot7b@aol.com>
2017-08-20 18:51:02 -0400

    :TheSaddest:
    wtf am i doing

--- src/main/java/com/witchworks/common/block/natural/tree/BlockWitchSapling.java
+++ src/main/java/com/witchworks/common/block/natural/tree/BlockWitchSapling.java
@@ -1,24 +1,13 @@
 package com.witchworks.common.block.natural.tree;
 
-import com.witchworks.client.handler.ModelHandler;
 import com.witchworks.common.block.BlockMod;
-import com.witchworks.common.block.natural.BlockGemOre;
 import com.witchworks.common.lib.LibMod;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 
@@ -33,9 +22,20 @@ public class BlockWitchSapling extends BlockMod {
 	@Override
 	public void getSubBlocks(@Nonnull CreativeTabs tab, @Nonnull NonNullList<ItemStack> items) {
 		{
-			for (int i = 0; i < BlockGemOre.Gem.values().length; ++i) {
+			for (int i = 0; i < TreeTypes.SaplingTypes.values().length; ++i) {
 				items.add(new ItemStack(this, 1, i));
 			}
 		}
+	}
+
+	public String getUnlocalizedName(ItemStack stack) {
+		for (int i = 0; i < TreeTypes.SaplingTypes.values().length; ++i) {
+			if (stack.getItemDamage() == i) {
+				return this.getUnlocalizedName() + "." + TreeTypes.SaplingTypes.values()[i].getName();
+			} else {
+				continue;
+			}
+		}
+		return this.getUnlocalizedName() + "." + TreeTypes.SaplingTypes.CYPRESS.getName();
 	}
 }
