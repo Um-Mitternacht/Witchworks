8d06cd3977438828195efe41466d0b7ec37a1354
Sunconure11 <eyespot7b@aol.com>
2017-08-21 17:51:29 -0400

    Half assed sapling attempt, for now

--- src/main/java/com/witchworks/common/block/natural/tree/BlockCypressSapling.java
+++ src/main/java/com/witchworks/common/block/natural/tree/BlockCypressSapling.java
@@ -7,12 +7,15 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static net.minecraft.block.BlockHorizontal.FACING;
 
@@ -50,6 +53,12 @@ public class BlockCypressSapling extends BlockMod {
 	@Override
 	public boolean isOpaqueCube(IBlockState state) {
 		return false;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer()
+	{
+		return BlockRenderLayer.TRANSLUCENT;
 	}
 
 	@SuppressWarnings("deprecation")
