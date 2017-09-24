fc3cb3d98aa025ccfc256fa7c1dff2b2d26c7216
Sunconure11 <eyespot7b@aol.com>
2017-09-24 04:17:14 -0400

    Oak apple logic now works on dark oak leaves only

--- src/main/java/com/witchworks/common/core/event/HarvestDropsEvent.java
+++ src/main/java/com/witchworks/common/core/event/HarvestDropsEvent.java
@@ -1,6 +1,8 @@
 package com.witchworks.common.core.event;
 
 import com.witchworks.common.item.ModItems;
+import net.minecraft.block.BlockNewLeaf;
+import net.minecraft.block.BlockPlanks;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent;
@@ -15,9 +17,9 @@ public class HarvestDropsEvent {
 	@SubscribeEvent
 	@Mod.EventHandler
 	public void onHarvestBlock(BlockEvent.HarvestDropsEvent event) {
-		if ((event.getState().getBlock() == Blocks.LEAVES2 && event.getWorld().rand.nextInt(100) < 55)) {
+		if ((event.getState().getBlock() == Blocks.LEAVES2.getDefaultState().withProperty(BlockNewLeaf.VARIANT, BlockPlanks.EnumType.DARK_OAK) && event.getWorld().rand.nextInt(100) < 55)) {
 			event.getDrops().clear();
-			event.getDrops().add(new ItemStack(ModItems.oak_apple_gall, 1));
+			event.getDrops().add(new ItemStack(ModItems.oak_apple_gall, 2));
 		}
 	}
 }
