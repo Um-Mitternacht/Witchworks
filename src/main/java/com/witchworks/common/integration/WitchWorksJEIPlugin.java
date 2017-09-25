4c9d10159dc4246bacab6b910cf69f307f5babfe
Sunconure11 <eyespot7b@aol.com>
2017-09-25 06:38:10 -0400

    JEI Plugin shell

---
+++ src/main/java/com/witchworks/common/integration/WitchWorksJEIPlugin.java
@@ -0,0 +1,29 @@
+package com.witchworks.common.integration;
+
+import com.witchworks.api.CauldronRegistry;
+import com.witchworks.common.block.ModBlocks;
+import com.witchworks.common.crafting.cauldron.CauldronCrafting;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created by Joseph on 9/25/2017.
+ */
+
+@JEIPlugin
+public class WitchWorksJEIPlugin implements IModPlugin {
+
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registry) {
+		registry.addRecipeCategories(new CauldronCategory(registry.getJeiHelpers().getGuiHelper()));
+	}
+
+	@Override
+	public void register (IModRegistry registry) {
+		registry.handleRecipes(CauldronRegistry.class, new CauldronWrapper, CauldronCategory.UID);
+		registry.addRecipeCatalyst(new ItemStack(ModBlocks.cauldron), CauldronCategory.UID);
+	}
+}
