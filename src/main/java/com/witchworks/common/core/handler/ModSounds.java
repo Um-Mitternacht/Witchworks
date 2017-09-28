a7cac2db3c8ee5ce2013f5d15e8fe4224bf501aa
Sunconure11 <eyespot7b@aol.com>
2017-09-28 07:17:01 -0400

    Fixed up sounds

--- src/main/java/com/witchworks/common/core/handler/ModSounds.java
+++ src/main/java/com/witchworks/common/core/handler/ModSounds.java
@@ -1,5 +1,10 @@
 package com.witchworks.common.core.handler;
 
+import com.witchworks.api.WitchWorksAPI;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
 /**
  * This class was created by <Arekkuusu> on 26/02/2017.
  * It's distributed as part of Witchworks under
@@ -12,12 +17,12 @@ public final class ModSounds {
 	private ModSounds() {
 	}
 
-	//public static void init() {
-	//	registerSound(WitchWorksAPI.BOIL);
-	//	registerSound(WitchWorksAPI.BUZZ);
-	//}
+	public static void init() {
+		registerSound(WitchWorksAPI.BOIL);
+		registerSound(WitchWorksAPI.BUZZ);
+	}
 
-	//private static void registerSound(ResourceLocation soundNameIn) {
-	//	GameRegistry.register(new SoundEvent(soundNameIn), soundNameIn);
-	//}
+	private static void registerSound(ResourceLocation soundNameIn) {
+		GameRegistry.register(new SoundEvent(soundNameIn), soundNameIn);
+	}
 }
