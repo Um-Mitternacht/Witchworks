892899ebeceb28d1933fea2dbd46ab89b44ef21c
Sunconure11 <eyespot7b@aol.com>
2017-10-06 14:39:23 -0400

    Made CauldronCrafting easier to read

--- src/main/java/com/witchworks/common/crafting/cauldron/CauldronCrafting.java
+++ src/main/java/com/witchworks/common/crafting/cauldron/CauldronCrafting.java
@@ -216,137 +216,203 @@ public final class CauldronCrafting {
 		//------------------------------------Custom Brew Creation------------------------------------//
 		registerEffect(getStack(Items.DYE, 1, 0)
 				, new PotionEffect(MobEffects.BLINDNESS, 500), false);
+
 		registerEffect(getStack(Items.ROTTEN_FLESH)
 				, new PotionEffect(MobEffects.HUNGER, 500), false);
+
 		registerEffect(getStack(Blocks.END_STONE)
 				, new PotionEffect(MobEffects.SLOWNESS, 500), false);
+
 		registerEffect(getStack(Items.SPIDER_EYE)
 				, new PotionEffect(MobEffects.POISON, 500), false);
+
 		registerEffect(getStack(Items.GHAST_TEAR)
 				, new PotionEffect(MobEffects.REGENERATION, 500), false);
+
 		registerEffect(getStack(Items.GOLDEN_CARROT)
 				, new PotionEffect(MobEffects.NIGHT_VISION, 500), false);
+
 		registerEffect(getStack(Items.SUGAR)
 				, new PotionEffect(MobEffects.SPEED, 500), false);
+
 		registerEffect(getStack(Items.MAGMA_CREAM)
 				, new PotionEffect(MobEffects.FIRE_RESISTANCE, 500), false);
+
 		registerEffect(getStack(Items.BLAZE_POWDER)
 				, new PotionEffect(MobEffects.STRENGTH, 500), false);
+
 		registerEffect(getStack(Items.RABBIT_FOOT)
 				, new PotionEffect(MobEffects.JUMP_BOOST, 500), false);
+
 		registerEffect(getStack(Items.SPECKLED_MELON)
 				, new PotionEffect(MobEffects.INSTANT_HEALTH, 1), false);
+
 		registerEffect(getStack(Items.FISH, 1, 3)
 				, new PotionEffect(MobEffects.WATER_BREATHING, 500), false);
+
 		registerEffect(getStack(Blocks.RED_FLOWER, 1, 1)
 				, new PotionEffect(MobEffects.LUCK, 500), false);
+
 		registerEffect(getStack(ModItems.wax)
 				, new PotionEffect(MobEffects.SLOWNESS, 500), false);
+
 		registerEffect(getStack(Items.FERMENTED_SPIDER_EYE)
 				, new PotionEffect(MobEffects.WEAKNESS, 500), false);
+
 		registerEffect(getStack(Items.POISONOUS_POTATO)
 				, new PotionEffect(MobEffects.NAUSEA, 500), false);
+
 		registerEffect(getStack(ModItems.belladonna)
 				, new PotionEffect(MobEffects.WITHER, 500), false);
+
 		registerEffect(getStack(ModItems.asphodel)
 				, new PotionEffect(MobEffects.UNLUCK, 500), false);
+
 		registerEffect(getStack(ModItems.lavender)
 				, new PotionEffect(MobEffects.HASTE, 500), false);
+
 		registerEffect(getStack(Items.PRISMARINE_CRYSTALS)
 				, new PotionEffect(MobEffects.GLOWING, 500), false);
+
 		registerEffect(getStack(Items.PRISMARINE_SHARD)
 				, new PotionEffect(MobEffects.MINING_FATIGUE, 500), false);
+
 		registerEffect(getStack(Items.SHULKER_SHELL)
 				, new PotionEffect(MobEffects.LEVITATION, 500), false);
+
 		registerEffect(getStack(Items.NETHER_STAR)
 				, new PotionEffect(MobEffects.RESISTANCE, 500), false);
+
 		registerEffect(getStack(ModBlocks.coquina)
 				, BrewRegistry.getDefault(ModBrews.SHELL_ARMOR), false);
+
 		registerEffect(getStack(ModItems.thistle)
 				, new PotionEffect(MobEffects.HEALTH_BOOST, 50), false);
+
 		registerEffect(getStack(Items.GOLDEN_APPLE)
 				, new PotionEffect(MobEffects.ABSORPTION, 50), false);
+
 		registerEffect(getStack(ModItems.heart)
 				, new PotionEffect(MobEffects.RESISTANCE, 50), false);
+
 		registerEffect(getStack(ModItems.mint)
 				, BrewRegistry.getDefault(ModBrews.EXTINGUISH_FIRES), false);
+
 		registerEffect(getStack(ModItems.white_sage)
 				, BrewRegistry.getDefault(ModBrews.HOLY_WATER), false);
+
 		registerEffect(getStack(Blocks.WEB)
 				, BrewRegistry.getDefault(ModBrews.SPIDER_NIGHTMARE), false);
+
 		registerEffect(getStack(Items.SNOWBALL)
 				, BrewRegistry.getDefault(ModBrews.FROSTBITE), false);
+
 		registerEffect(getStack(Blocks.TNT)
 				, BrewRegistry.getDefault(ModBrews.VOLATILE), false);
+
 		registerEffect(getStack(ModItems.aconitum)
 				, BrewRegistry.getDefault(ModBrews.WOLFSBANE), false);
+
 		registerEffect(getStack(ModItems.wormwood)
 				, BrewRegistry.getDefault(ModBrews.BANE_ARTHROPODS), false);
+
 		registerEffect(getStack(ModItems.kelp)
 				, BrewRegistry.getDefault(ModBrews.PATH_OF_THE_DEEP), false);
+
 		registerEffect(getStack(ModItems.silphium)
 				, BrewRegistry.getDefault(ModBrews.GROW_FLOWER), false);
+
 		registerEffect(getStack(ModItems.seed_silphium)
 				, BrewRegistry.getDefault(ModBrews.HARVEST), false);
+
 		registerEffect(getStack(ModItems.dimensional_sand)
 				, BrewRegistry.getDefault(ModBrews.ENDER_INHIBITION), false);
+
 		registerEffect(getStack(ModItems.gem, 1, 8)
 				, BrewRegistry.getDefault(ModBrews.TILL_LAND), false);
+
 		registerEffect(getStack(ModItems.gem, 1, 6)
 				, BrewRegistry.getDefault(ModBrews.ROCK_PULVERIZE), false);
+
 		registerEffect(getStack(Items.DYE, 1, 15)
 				, BrewRegistry.getDefault(ModBrews.FERTILIZE), false);
+
 		registerEffect(getStack(Blocks.PACKED_ICE)
 				, BrewRegistry.getDefault(ModBrews.SNOW_TRAIL), false);
+
 		registerEffect(getStack(Items.IRON_NUGGET)
 				, BrewRegistry.getDefault(ModBrews.SINKING), false);
+
 		registerEffect(getStack(Blocks.BROWN_MUSHROOM)
 				, BrewRegistry.getDefault(ModBrews.PRUNE_LEAVES), false);
+
 		registerEffect(getStack(Blocks.RED_MUSHROOM)
 				, BrewRegistry.getDefault(ModBrews.AUTO_PLANT), false);
+
 		registerEffect(getStack(ModItems.ginger)
 				, BrewRegistry.getDefault(ModBrews.IGNITION), false);
+
 		registerEffect(getStack(ModItems.carnivorous_tooth)
 				, BrewRegistry.getDefault(ModBrews.OUTCASTS_SHAME), false);
+
 		registerEffect(getStack(ModItems.wool_of_bat)
 				, BrewRegistry.getDefault(ModBrews.GRACE), false);
+
 		registerEffect(getStack(ModItems.tulsi)
 				, BrewRegistry.getDefault(ModBrews.PURIFY), false);
+
 		registerEffect(getStack(Items.GOLDEN_APPLE, 1, 1)
 				, BrewRegistry.getDefault(ModBrews.NOTCHED), false);
+
 		registerEffect(getStack(Blocks.MYCELIUM)
 				, BrewRegistry.getDefault(ModBrews.MYCOLOGICAL_CORRUPTION), false);
+
 		registerEffect(getStack(Blocks.GRASS)
 				, BrewRegistry.getDefault(ModBrews.GROWTH), false);
+
 		registerEffect(getStack(Blocks.MOSSY_COBBLESTONE)
 				, BrewRegistry.getDefault(ModBrews.OZYMANDIAS), false);
+
 		registerEffect(getStack(Blocks.RED_NETHER_BRICK)
 				, BrewRegistry.getDefault(ModBrews.HELLS_WROTH), false);
+
 		registerEffect(getStack(Blocks.SAND, 1, 1)
 				, BrewRegistry.getDefault(ModBrews.SETEHS_WASTES), false);
+
 		registerEffect(getStack(ModBlocks.nethersteel)
 				, BrewRegistry.getDefault(ModBrews.HELL_WORLD), false);
+
 		registerEffect(getStack(ModItems.seed_mint)
 				, BrewRegistry.getDefault(ModBrews.ICE_WORLD), false);
+
 		registerEffect(getStack(Items.CHORUS_FRUIT)
 				, BrewRegistry.getDefault(ModBrews.CURSED_LEAPING), false);
+
 		registerEffect(getStack(Items.BONE)
 				, BrewRegistry.getDefault(ModBrews.CORRUPTION), false);
+
 		registerEffect(getStack(ModItems.salt)
 				, BrewRegistry.getDefault(ModBrews.SALT_LAND), false);
+
 		registerEffect(getStack(ModItems.silver_scales)
 				, BrewRegistry.getDefault(ModBrews.BULLETPROOF), false);
 
+
 		registerModifier(getStack(Items.QUARTZ)
 				, new BrewSimpleModifier(2400, 0), true);
+
 		registerModifier(getStack(Items.REDSTONE)
 				, new BrewSimpleModifier(600, 0), true);
+
 		registerModifier(getStack(Blocks.REDSTONE_BLOCK)
 				, new BrewSimpleModifier(1200, 0), true);
+
 		registerModifier(getStack(ModItems.gem, 1, 2)
 				, new BrewSimpleModifier(0, 3), true);
+
 		registerModifier(getStack(Items.GLOWSTONE_DUST)
 				, new BrewSimpleModifier(0, 1), true);
+		
 		registerModifier(getStack(Blocks.GLOWSTONE)
 				, new BrewSimpleModifier(0, 2), true);
 	}
