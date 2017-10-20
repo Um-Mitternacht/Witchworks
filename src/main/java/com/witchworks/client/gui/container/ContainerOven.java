6d9e96c421ece01494c3dd7edce657a99121b926
Sunconure11 <eyespot7b@aol.com>
2017-10-20 04:34:36 -0400

    slapped bits of apiary code onto the oven so i can rework it to function for the oven

--- src/main/java/com/witchworks/client/gui/container/ContainerOven.java
+++ src/main/java/com/witchworks/client/gui/container/ContainerOven.java
@@ -1,11 +1,16 @@
 package com.witchworks.client.gui.container;
 
+import com.witchworks.common.item.ModItems;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IContainerListener;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+
+import javax.annotation.Nullable;
 
 /**
  * Created by Joseph on 7/17/2017.
@@ -15,8 +20,30 @@ public class ContainerOven extends Container {
 	//Todo: Continue, life is getting in the way, as well as a damaged sleep cycle.
 	private final IInventory oven;
 
+
 	public ContainerOven(InventoryPlayer playerInventory, IInventory inventory) {
 		this.oven = inventory;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 6; j++) {
+				this.addSlotToContainer(new ContainerOven.SlotItem(inventory, j + i * 6 + 1, 62 + j * 18, 16 + i * 18));
+			}
+		}
+
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 9; ++j) {
+				this.addSlotToContainer(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+			}
+		}
+
+		for (int k = 0; k < 9; ++k) {
+			this.addSlotToContainer(new Slot(playerInventory, k, 8 + k * 18, 142));
+		}
+	}
+
+	public void addListener(IContainerListener listener) {
+		super.addListener(listener);
+		listener.sendAllWindowProperties(this, this.oven);
 	}
 
 	@Override
@@ -58,8 +85,14 @@ public class ContainerOven extends Container {
 		return !playerIn.isSpectator();
 	}
 
+	private class SlotItem extends Slot {
 
-	public int getItemStackLimit(ItemStack stack) {
-		return 64;
+		SlotItem(IInventory inventoryIn, int slotIndex, int x, int y) {
+			super(inventoryIn, slotIndex, x, y);
+		}
+
+		public int getItemStackLimit(ItemStack stack) {
+			return 64;
+		}
 	}
 }
