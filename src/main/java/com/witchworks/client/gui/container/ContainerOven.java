8e86cd3d0000832a872b3baf7787d71ee6674127
Sunconure11 <eyespot7b@aol.com>
2017-10-20 04:38:13 -0400

    Revert "slapped bits of apiary code onto the oven so i can rework it to function for the oven"
    This reverts commit 6d9e96c421ece01494c3dd7edce657a99121b926.

--- src/main/java/com/witchworks/client/gui/container/ContainerOven.java
+++ src/main/java/com/witchworks/client/gui/container/ContainerOven.java
@@ -3,7 +3,6 @@ package com.witchworks.client.gui.container;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
-import net.minecraft.inventory.IContainerListener;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
@@ -16,30 +15,8 @@ public class ContainerOven extends Container {
 	//Todo: Continue, life is getting in the way, as well as a damaged sleep cycle.
 	private final IInventory oven;
 
-
 	public ContainerOven(InventoryPlayer playerInventory, IInventory inventory) {
 		this.oven = inventory;
-
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 6; j++) {
-				this.addSlotToContainer(new ContainerOven.SlotItem(inventory, j + i * 6 + 1, 62 + j * 18, 16 + i * 18));
-			}
-		}
-
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 9; ++j) {
-				this.addSlotToContainer(new Slot(playerInventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
-			}
-		}
-
-		for (int k = 0; k < 9; ++k) {
-			this.addSlotToContainer(new Slot(playerInventory, k, 8 + k * 18, 142));
-		}
-	}
-
-	public void addListener(IContainerListener listener) {
-		super.addListener(listener);
-		listener.sendAllWindowProperties(this, this.oven);
 	}
 
 	@Override
@@ -81,14 +58,8 @@ public class ContainerOven extends Container {
 		return !playerIn.isSpectator();
 	}
 
-	private class SlotItem extends Slot {
 
-		SlotItem(IInventory inventoryIn, int slotIndex, int x, int y) {
-			super(inventoryIn, slotIndex, x, y);
-		}
-
-		public int getItemStackLimit(ItemStack stack) {
-			return 64;
-		}
+	public int getItemStackLimit(ItemStack stack) {
+		return 64;
 	}
 }
