3593b23a03cefbbf2fa3cbf6e4ea66d857d30486
Sunconure11 <eyespot7b@aol.com>
2017-11-05 15:22:23 -0500

    Torchwood should not be placeable on itself
    Need to get it to spread a bit more obviously for testing purposes later

--- src/main/java/com/witchworks/common/block/magic/plants/BlockTorchwood.java
+++ src/main/java/com/witchworks/common/block/magic/plants/BlockTorchwood.java
@@ -10,17 +10,22 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
 /**
  * Created by Joseph on 11/4/2017.
  */
-public class BlockTorchwood extends BlockMod implements IGrowable {
+public class BlockTorchwood extends BlockMod implements IGrowable, IPlantable {
 
 	public BlockTorchwood() {
 		super(LibBlockName.TORCHWOOD, Material.WOOD);
@@ -51,16 +56,38 @@ public class BlockTorchwood extends BlockMod implements IGrowable {
 		return state.getBlock() == Blocks.GRASS || state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.STONE || state.getBlock() == Blocks.FARMLAND || state.getBlock() == ModBlocks.chalk || state.getBlock() == ModBlocks.coquina || state.getBlock() == Blocks.SAND || state.getBlock() == Blocks.GRAVEL || state.getBlock() == this;
 	}
 
+	protected void checkAndDropBlock(World worldIn, BlockPos pos, IBlockState state) {
+		if (!this.canBlockStay(worldIn, pos, state)) {
+			this.dropBlockAsItem(worldIn, pos, state, 0);
+			worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+		}
+	}
+
+	@Override
+	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
+		super.neighborChanged(state, worldIn, pos, blockIn, fromPos);
+		this.checkAndDropBlock(worldIn, pos, state);
+	}
+
 	@Override
 	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
-		IBlockState state = worldIn.getBlockState(pos.down());
-		return canSustainBush(state);
+		IBlockState soil = worldIn.getBlockState(pos.down());
+		return super.canPlaceBlockAt(worldIn, pos)
+				&& soil.getBlock().canSustainPlant(soil, worldIn, pos.down(), net.minecraft.util.EnumFacing.UP, this);
+	}
+
+	public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
+		if (state.getBlock() == this) {
+			IBlockState soil = worldIn.getBlockState(pos.down());
+			return soil.getBlock().canSustainPlant(soil, worldIn, pos.down(), net.minecraft.util.EnumFacing.UP, this);
+		}
+		return this.canSustainBush(worldIn.getBlockState(pos.down()));
 	}
 
 	@Override
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
 		super.updateTick(worldIn, pos, state, rand);
-		if (rand.nextInt(42) == 0) {
+		if (rand.nextInt(25) == 0) {
 			worldIn.setBlockState(pos, getDefaultState());
 		} else {
 			trySpread(worldIn, pos, rand);
@@ -90,6 +117,11 @@ public class BlockTorchwood extends BlockMod implements IGrowable {
 		return Block.EnumOffsetType.XYZ;
 	}
 
+	@Nullable
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
+	{
+		return NULL_AABB;
+	}
 
 	@Override
 	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
@@ -99,5 +131,15 @@ public class BlockTorchwood extends BlockMod implements IGrowable {
 	@Override
 	public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
 
+	}
+
+	@Override
+	public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
+		return EnumPlantType.Plains;
+	}
+
+	@Override
+	public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
+		return this.getDefaultState();
 	}
 }
