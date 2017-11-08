fd3c3e1e045efa6460d29b608912491ae7d1ede6
Sunconure11 <eyespot7b@aol.com>
2017-11-08 12:49:13 -0500

    Updated API version, in preparation for the oven

--- src/main/java/com/witchworks/api/package-info.java
+++ src/main/java/com/witchworks/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "witchworks", apiVersion = "0.5", provides = "WitchWorksAPI")
+@API(owner = "witchworks", apiVersion = "0.6", provides = "WitchWorksAPI")
 package com.witchworks.api;
 
 import net.minecraftforge.fml.common.API;
