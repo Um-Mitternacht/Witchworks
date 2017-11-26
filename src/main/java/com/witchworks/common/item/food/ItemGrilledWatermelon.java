65810d052111fd17b87369d8166acc19e2dc1c5a
Sunconure11 <eyespot7b@aol.com>
2017-11-26 05:13:32 -0500

    Reformat

--- src/main/java/com/witchworks/common/item/food/ItemGrilledWatermelon.java
+++ src/main/java/com/witchworks/common/item/food/ItemGrilledWatermelon.java
@@ -2,11 +2,6 @@ package com.witchworks.common.item.food;
 
 import com.witchworks.common.core.WitchWorksCreativeTabs;
 import com.witchworks.common.lib.LibItemName;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.MobEffects;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.world.World;
 
 /**
  * This class was created by Arekkuusu on 03/07/2017, and modified by Sunconure11 on 03/17/2017.
